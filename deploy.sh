e6afbc1a9d40c5e743cbbc79f43ffd5bdc41817b
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-11-22 20:48:22 +0530

    Added KOSS page

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-python3 fbscraper.py
+python3 /Users/ayush/scripts/naarad-source/fbscraper.py
 #python3 frontend.py
 
 # Commit changes
-echo "commiting.."
-git add -A
-git commit -m 'Update and Build'
+#echo "commiting.."
+#git add -A
+#git commit -m 'Update and Build'
 
 # Create the subtree, push changes to gh-pages
 # git subtree split --prefix output -b gh-pages
 
-echo "deploying.."
-git push origin master
+#echo "deploying.."
+#git push origin master
 # git push -f origin gh-pages:gh-pages
 
 # echo "cleaning..."
